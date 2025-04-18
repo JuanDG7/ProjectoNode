f9ec1b4a34f81da6dc9331319ce30ba876949bae
Juan Duarte <142144090+JuanDG7@users.noreply.github.com>
2025-04-18 10:04:27 -0700

    hotfix

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(
   session({
-    secret: process.env.SESSION_KEY,
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     store: store,
